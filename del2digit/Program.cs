d30d7022fec26e761b2257d2f7e77af3861ad6e2
Alina Malyutina <linamalutina1@gmail.com>
2022-11-03 23:17:31 +0300

    add 11 task

---
+++ del2digit/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = new Random().Next(100, 1000);
+
+int hundreds = number / 100;
+hundreds = hundreds * 10;
+int LastDigit = number % 10;
+
+int NumberEnd = hundreds + LastDigit;
+
+Console.WriteLine($"Random number is {number}");
+Console.WriteLine($"Result is {NumberEnd}");
